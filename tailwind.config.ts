acd9ab2921512b1d7a5fd9910f65eb6b38963426
Brian West <WestBrian@users.noreply.github.com>
2025-01-19 14:52:46 -0500

    🗑️  Remove boilerplate code (#3)

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,7 +2,6 @@ import type { Config } from "tailwindcss";
 
 export default {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
@@ -11,6 +10,10 @@ export default {
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
+      },
+      fontFamily: {
+        sans: ["var(--font-geist-sans)", "ui-sans-serif", "system-ui"],
+        mono: ["var(--font-geist-mono)", "ui-monospace", "SFMono-Regular"],
       },
     },
   },
